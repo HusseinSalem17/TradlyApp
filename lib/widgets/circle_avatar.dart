f94d62b0d1395020c15729ced8363e182e9debad
AhmedRamy2000 <125711699+AhmedRamy2000@users.noreply.github.com>
2023-09-03 17:15:08 +0300

    build onboarding view part 2

---
+++ lib/widgets/circle_avatar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:tradly_app/constants.dart';
+
+class circleAvatar extends StatelessWidget {
+  const circleAvatar({super.key, this.isActive = false});
+  final bool isActive;
+
+  @override
+  Widget build(BuildContext context) {
+    return isActive
+        ? const CircleAvatar(
+            radius: 8,
+            backgroundColor: kprimaryColor,
+          )
+        :  CircleAvatar(
+            radius: 8,
+            backgroundColor: kprimaryColor.withOpacity(0.4),
+          );
+  }
+}
