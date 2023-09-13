9df3c1c2c770a2db4da54baf908d2ea6b86288ca
AhmedRamy2000 <125711699+AhmedRamy2000@users.noreply.github.com>
2023-09-13 03:09:42 +0300

    refactoring navigation bar & add faetures files

---
+++ lib/features/Store_feature/store_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StoreView extends StatelessWidget {
+  const StoreView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-
 import 'package:tradly_app/core/utils/app_theme.dart';
-import 'package:tradly_app/features/auth_feature/presentation/views/send_OTP.dart';
-import 'package:tradly_app/features/auth_feature/presentation/views/widgets/Verify.dart';
-import 'package:tradly_app/features/home_feature/presentation/views/home_screen.dart';
+import 'package:tradly_app/features/home_feature/presentation/views/my_home_screen.dart';
 import 'package:tradly_app/routes.dart';
-import 'core/onboarding_screen/views/onBoardviews.dart';
-import 'core/splash/presentation/views/splash_screen.dart';
 
 void main() {
   runApp(const TradlyApp());
@@ -20,7 +15,7 @@ class TradlyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: Themes.CustomlightTheme,
-      home:  HomeScreen(),
+      home:  MyHomePage(),
       onGenerateRoute: (settings) => generateRoute(settings),
     );
   }
